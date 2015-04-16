6b8166687e4613ee5bf9e9aa8126e0bdbbaef08c
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2015-04-17 01:33:21 +0200

    Initial project creation

---
+++ Game-Of-Life/Game-Of-Life/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Game-Of-Life
+//
+//  Created by Bartlomiej Guminiak on 17/04/15.
+//  Copyright (c) 2015 Bartlomiej Guminiak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Game-Of-Life/Game-Of-Life/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Game-Of-Life
+//
+//  Created by Bartlomiej Guminiak on 17/04/15.
+//  Copyright (c) 2015 Bartlomiej Guminiak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
