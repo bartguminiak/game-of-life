73afc5ff268f9a46c5cde23a18d00312fc7e0330
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2015-04-17 11:39:59 +0200

    Added cell and live board

---
+++ Game-Of-Life/Game-Of-Life/GOLCoreLogic/GOLLiveBoard.h
@@ -0,0 +1,10 @@
+//
+// Created by Bartlomiej Guminiak on 17/04/15.
+// Copyright (c) 2015 Bartlomiej Guminiak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GOLLiveBoard : NSObject
+@end
---
+++ Game-Of-Life/Game-Of-Life/GOLCoreLogic/GOLLiveBoard.m
@@ -0,0 +1,12 @@
+//
+// Created by Bartlomiej Guminiak on 17/04/15.
+// Copyright (c) 2015 Bartlomiej Guminiak. All rights reserved.
+//
+
+#import "GOLLiveBoard.h"
+
+
+@implementation GOLLiveBoard {
+
+}
+@end
