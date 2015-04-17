c907f5baed9b7154acd7fadc0ca7d746181114a8
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2015-04-17 12:09:45 +0200

    Test configuration

---
+++ Game-Of-Life/Game-Of-LifeTests/Components/GOLLiveCellSpec.m
@@ -0,0 +1,26 @@
+//
+// Created by Bartlomiej Guminiak on 17/04/15.
+// Copyright (c) 2015 Bartlomiej Guminiak. All rights reserved.
+//
+
+#import "GOLSpecHelper.h"
+
+SpecBegin(DOCDoctorProfileServicesViewModel)
+
+    describe(@"servicesViewModel", ^{});
+
+    it(@"TEST", ^{
+        int a = 5;
+        expect(a).to.equal(5);
+    });
+//beforeEach(^{
+//servicesViewModel = [[DOCDoctorProfileServicesViewModel alloc] initWithDoctor:doctor];
+//});
+
+//it(@"Number of rows for header", ^{
+//expect(servicesViewModel.numberOfRowsHeader).to.equal(1);
+//});
+
+
+
+SpecEnd
